c591ee54c9d44bdb3721bd3e9949627aa76bf648
Anshul Shah <anshulshah@Anshuls-MacBook-Air.local>
2025-04-02 01:32:16 -0400

    set up cors and json parsing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
-import express from "express";
+
+
+import express, { json } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv"; 
+
+// load env variables from .env file
+dotenv.config();
+
+// initialize express app
+const app = express();
+
+// enable Cors to let front and backend talk to each other
+app.use(cors());
+
+// enable json parsing for incoming
+app.use(express.json());
