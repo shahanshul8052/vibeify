ebe1ab90c15f8c3180b4d99a2bdf804a6aa7f1d3
Anshul Shah <anshulshah@Anshuls-MacBook-Air.local>
2025-04-02 01:34:26 -0400

    set up working api server that parses data, responds requests, connects mongodb, handles errors

--- server/index.js
+++ server/index.js
@@ -16,3 +16,24 @@ app.use(cors());
 
 // enable json parsing for incoming
 app.use(express.json());
+
+// test route to check if server is running
+app.get("/", (req, res) => {
+    res.send("Server is running");
+    });
+
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("Connected to MongoDB");
+
+    // start server
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server running on port ${process.env.PORT || 5000}`);
+      });
+    })
+    .catch(err => console.error("MongoDB connection error:", err));
