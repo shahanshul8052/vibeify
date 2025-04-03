e3f5e863477e3e7e39e88eadfa9cde6301dcac7d
Anshul Shah <anshulshah@Anshuls-MacBook-Air.local>
2025-04-02 23:50:02 -0400

    added dashboard link to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 const App: React.FC = () => {
   console.log("App loaded ✅");
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
